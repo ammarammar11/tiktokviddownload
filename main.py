8d61310305387c98fab1abf534492ea33f766ad3
Vyacheslav1557 <slavabirin1@yandex.ru>
2025-07-01 00:59:11 +0500

    fix: change handlers apply order

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ dotenv.load_dotenv()
 def main() -> None:
     app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
 
+    app.add_handler(CommandHandler("all", handle_all))
+    app.add_handler(MessageHandler(filters.Regex(re.compile(r'@all', re.IGNORECASE)), handle_all))
+
     app.add_handler(CommandHandler("roll", roll))
     app.add_handler(CommandHandler("choose", choose))
     app.add_handler(CommandHandler("ch", choose))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-
-    app.add_handler(CommandHandler("all", handle_all))
-    app.add_handler(MessageHandler(filters.Regex(re.compile(r'@all', re.IGNORECASE)), handle_all))
 
     logger.info("Bot started")
     app.run_polling()
